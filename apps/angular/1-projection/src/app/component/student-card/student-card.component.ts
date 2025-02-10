ae2ce5ef6e33f5144c916dc0a92f28024c04aff1
arrKhange1 <therock65frostik@gmail.com>
2025-02-10 18:24:12 +0500

    refactor: make img as ng-content

--- apps/angular/1-projection/src/app/component/student-card/student-card.component.ts
+++ apps/angular/1-projection/src/app/component/student-card/student-card.component.ts
@@ -1,21 +1,35 @@
+import { NgOptimizedImage } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
   inject,
   OnInit,
 } from '@angular/core';
-import { FakeHttpService } from '../../data-access/fake-http.service';
+import {
+  FakeHttpService,
+  randStudent,
+} from '../../data-access/fake-http.service';
 import { StudentStore } from '../../data-access/student.store';
-import { CardType } from '../../model/card.model';
 import { CardComponent } from '../../ui/card/card.component';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
   selector: 'app-student-card',
   template: `
+    <ng-template #studentListItem let-item>
+      <app-list-item
+        (deleteItem)="deleteStudent($event)"
+        [id]="item.id"
+        [name]="item.firstName"></app-list-item>
+    </ng-template>
+
     <app-card
+      (addItem)="addStudent()"
+      [listItemTemplate]="studentListItem"
       [list]="students()"
-      [type]="cardType"
-      customClass="bg-light-green" />
+      customClass="bg-light-green">
+      <img ngSrc="assets/img/student.webp" width="200" height="200" />
+    </app-card>
   `,
   styles: [
     `
@@ -24,7 +38,7 @@ import { CardComponent } from '../../ui/card/card.component';
       }
     `,
   ],
-  imports: [CardComponent],
+  imports: [CardComponent, NgOptimizedImage, ListItemComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StudentCardComponent implements OnInit {
@@ -32,9 +46,16 @@ export class StudentCardComponent implements OnInit {
   private store = inject(StudentStore);
 
   students = this.store.students;
-  cardType = CardType.STUDENT;
 
   ngOnInit(): void {
     this.http.fetchStudents$.subscribe((s) => this.store.addAll(s));
+  }
+
+  protected addStudent(): void {
+    this.store.addOne(randStudent());
+  }
+
+  protected deleteStudent(id: number): void {
+    this.store.deleteOne(id);
   }
 }
